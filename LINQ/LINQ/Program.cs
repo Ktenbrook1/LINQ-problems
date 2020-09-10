f76d34d7570da68b57c7a2663b9e7f8ad1da19ae
ktenbrook1 <katelynntenbrook@gmail.com>
2020-09-10 15:35:59 -0500

    2 complete, Will only print distinct names

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -10,13 +10,20 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathmatics" };
-            var results = words.Where(w => w.Contains("th"));
-            foreach(var result in results)
+            //List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathmatics" };
+            //var results = words.Where(w => w.Contains("th"));
+            //foreach(var result in results)
+            //{
+            //    Console.WriteLine(result);
+            //}          
+            //Console.ReadLine();
+
+            List<string> names = new List<string>() { "Mike", "Charles", "Nevin", "David", "Mike", "Tony", "Brett"};
+            var distinctNames = names.Distinct();
+            foreach (var name in distinctNames)
             {
-                Console.WriteLine(result);
+                Console.WriteLine(name);
             }
-          
             Console.ReadLine();
         }
     }
