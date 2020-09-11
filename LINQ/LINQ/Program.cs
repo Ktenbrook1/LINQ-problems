94134c66b768bb5ea0925383177c1b7a045783ae
ktenbrook1 <katelynntenbrook@gmail.com>
2020-09-11 09:02:06 -0500

    fourth problem done need to work on 3rd

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -18,13 +18,40 @@ namespace LINQ
             //}          
             //Console.ReadLine();
 
-            List<string> names = new List<string>() { "Mike", "Charles", "Nevin", "David", "Mike", "Tony", "Brett"};
-            var distinctNames = names.Distinct();
-            foreach (var name in distinctNames)
+            //List<string> names = new List<string>() { "Mike", "Charles", "Nevin", "David", "Mike", "Tony", "Brett"};
+            //var distinctNames = names.Distinct();
+            //foreach (var name in distinctNames)
+            //{
+            //    Console.WriteLine(name);
+            //}
+            //Console.ReadLine();
+
+            List<string> classGrades = new List<string>()
             {
-                Console.WriteLine(name);
+            "80,100,92,89,65",
+            "93,81,78,84,69",
+            "73,88,83,99,64",
+            "98,100,66,74,55"
+            };
+            string[] grades;
+            foreach (var stringOfGrades in classGrades)
+            {
+                grades = stringOfGrades.Split(',');
+             
             }
-            Console.ReadLine();
+          
+
+            //string name = "Terrill";
+
+            //var organized = name.ToLower().OrderBy(n => n).GroupBy(n => n).Select(n => n);
+
+            //foreach (var countOfCharacters in organized)
+            //{
+            //    Console.Write("" + countOfCharacters.Key + countOfCharacters.Count());
+            //}
+            //Console.ReadLine();
+
+
         }
     }
 }
